eaa2aeebee76ee754d78da84e351304eff3cf75e
vishesh faujdar <visheshfaujdar91@gmail.com>
2025-09-04 21:27:27 +0530

    Create number is Negative, or Positive or Zero.c

---
+++ number is Negative, or Positive or Zero.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int a ;
+    printf("enter your number\n");
+    scanf("%d",&a);
+    if(a>0){
+        printf("number is positive\n");
+        }
+        else if(a==0){
+            printf("number is zero\n");
+        }
+        else{
+            printf("number is negative\n");
+        }
+return 0 ;
+    }
