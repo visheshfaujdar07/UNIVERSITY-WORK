9d84337ae15bfbb412c86b247a38d1a59dd6f263
vishesh faujdar <visheshfaujdar91@gmail.com>
2025-09-04 20:38:49 +0530

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float base, height, area;
+
+    printf("Enter base: ");
+    scanf("%f", &base);
+
+    printf("Enter height: ");
+    scanf("%f", &height);
+
+    area = 0.5 * base * height;
+
+    printf("Area of the triangle = %.2f\n", area);
+
+    return 0;
+}
