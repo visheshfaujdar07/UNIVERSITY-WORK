6e6c8c6d3d12d9c11545974d089628d94fe1e0a8
vishesh faujdar <visheshfaujdar91@gmail.com>
2025-09-04 21:04:09 +0530

    Create (distance conversion).c

---
+++ (distance conversion).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    float km, meter, feet, inch, cm;
+
+    printf("Enter distance in kilometers: ");
+    scanf("%f", &km);
+
+    meter = km * 1000;
+    cm = meter * 100;
+    inch = cm / 2.54;
+    feet = inch / 12;
+
+    printf("Distance in meters = %.2f m\n", meter);
+    printf("Distance in centimeters = %.2f cm\n", cm);
+    printf("Distance in inches = %.2f in\n", inch);
+    printf("Distance in feet = %.2f ft\n", feet);
+
+    return 0;
+}
