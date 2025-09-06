26c4dbfb3e64ab0eb6344f085a29a4366b28872e
vishesh faujdar <visheshfaujdar91@gmail.com>
2025-09-06 12:27:36 +0530

    Create sum of first and last digit of a number.c

---
+++ sum of first and last digit of a number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int n ;
+    printf("enter your number ");
+    scanf("%d",&n);
+    int z = n%10;
+    for( n ;n>=10;n=n/10){
+        
+    }
+    printf("%d",n+z);
+    return 0 ;
+}
