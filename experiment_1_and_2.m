d514c20f296ee4870085bcaac4af12e96de60593
vishesh faujdar <visheshfaujdar91@gmail.com>
2025-09-13 12:16:12 +0530

    Add files via upload

---
+++ experiment_1_and_2.m
@@ -0,0 +1,25 @@
+% to add numbers 
+a=2+2 ;
+b = 3+2 ; 
+a , b 
+
+% to find square roots 
+ c = sqrt(a)
+
+ % to write matrix 
+ 
+ A = [1 2 ; 5 6] ;
+ A 
+ 
+ %to write transpose of matrix
+
+ A'
+ 
+ % we can also multipy matrices like this :
+ B = [3 0 ; 1 2] ;
+ A*B
+
+ % use of colon operator 
+ x = 1:5 
+ y = 1:2:10
+ % here in 'y' 2 is used as an increament per number before 10
