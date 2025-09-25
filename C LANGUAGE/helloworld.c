aa8ba228cfeb29d94ad6a50d17d1e511e8fd13d3
vishesh faujdar <visheshfaujdar91@gmail.com>
2025-09-25 08:10:44 +0530

    Add files via upload

---
+++ C LANGUAGE/helloworld.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int year;
+
+    // Ask user to input a year
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    // Leap year conditions
+    if (year % 400 == 0) {
+        printf("%d is a leap year.\n", year);
+    }
+    else if (year % 100 == 0) {
+        printf("%d is not a leap year.\n", year);
+    }
+    else if (year % 4 == 0) {
+        printf("%d is a leap year.\n", year);
+    }
+    else {
+        printf("%d is not a leap year.\n", year);
+    }
+
+    return 0;
+}
